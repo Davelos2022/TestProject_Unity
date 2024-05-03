d1ccf2742daadb8d7616dd96ced9542cfc781e78
Denis <108666402+Davelos2022@users.noreply.github.com>
2024-05-03 23:46:59 +0300

    fix

--- Assets/Scripts/Data/DataModel.cs
+++ Assets/Scripts/Data/DataModel.cs
@@ -3,7 +3,6 @@ namespace TestProject.Data
     public class DataModel
     {
         private int[,] _data;
-
         public int[,] Data => _data;
 
         public void LoadData(IDataLoader dataLoader, string nameFile)
